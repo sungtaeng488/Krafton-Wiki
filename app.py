8f4cec46cad0ae2e9c06e3406a03628595d9ace6
ldcldc <1qw23er45ty08@gmail.com>
2026-08-25 23:40:51 +0900

    _

--- app.py
+++ app.py
@@ -1,11 +1,17 @@
 from flask import Flask
 
-from routes.main import main_bp
-
+from routes import all_blueprints 
 
 app = Flask(__name__)
-app.register_blueprint(main_bp)
+
+app.config["SECRET_KEY"] = "qwertyuiop" # flash용 SECRET_KEY설정.
+
+for bp in all_blueprints:
+    app.register_blueprint(bp)
 
 
-if __name__ == "__main__":
-    app.run(debug=True)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5001, debug=True)
+
+
+
---
+++ routes/post.py
@@ -0,0 +1,31 @@
+from flask import Flask, Blueprint, render_template, redirect, url_for, request
+from bson.objectid import ObjectId
+from .database import db
+
+post_bp = Blueprint("post", __name__)
+
+
+@post_bp.route('/post/<id>')                            #post 페이지
+def view_post(id):
+    # DB에서 해당 ID의 post post.html로 전송
+    post = db.posts.find_one({'_id': ObjectId(id)})
+    return render_template('post.html', post=post)
+
+@post_bp.route('/like/<id>', methods=['POST'])          #좋아요
+def like_post(id):
+    #likes++ 후 #view post로
+    db.posts.update_one({'_id': ObjectId(id)}, {'$inc': {'likes': 1}})
+    return redirect(url_for('view_post', id=id))
+
+@post_bp.route('/comment/<id>', methods=['POST'])       #댓글
+def add_comment(id):
+    author = request.form.get('author')
+    text = request.form.get('text')
+    
+    if text:
+        #작성 댓글을 comments 리스트에 push
+        db.posts.update_one(
+            {'_id': ObjectId(id)},
+            {'$push': {'comments': {'author': author, 'text': text}}}
+        )
+    return redirect(url_for('view_post', id=id))
