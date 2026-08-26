ac96198f397731f5a6baf9eda5c4a29f4e597383
이종민 <145272576+steinsring@users.noreply.github.com>
2026-08-26 11:36:56 +0900

    fix : connect jwt to post
    임시 작성, 수정 페이지 삭제
    write.py와 연결
    write.py redirct오류 해결
    write.py에 edit : 로그인 검증 기능 추가
    post db 통일
    비로그인시 수정 버튼 숨김

--- routes/write.py
+++ routes/write.py
@@ -1,7 +1,11 @@
-from flask import Flask, request, Blueprint, render_template, flash, redirect, url_for
+from datetime import datetime, timezone
+
+from bson.errors import InvalidId
+from flask import Blueprint, abort, flash, g, redirect, render_template, request, url_for
 from bson.objectid import ObjectId
-from datetime import datetime
 from .database import db
+
+from routes.authority import login_required
 
 write_bp = Blueprint("write", __name__)
 
@@ -26,10 +30,12 @@ def get_post_form_data():
         'likes': 0,         # 기본값 추가
         'comments': [],      # 기본값 추가
         'current_time': datetime.now().strftime('%Y-%m-%d %H:%M'),
-        'author': request.form.get('author')
+        "user_id": g.user_id,   # user id
+        #"author": g.user_id,    기존 화면 호환용, 제거가능
     }
 
 @write_bp.route('/write', methods=['GET', 'POST'])       #새 글 작성
+@login_required
 def write():
     if request.method == 'POST':
         form_data = get_post_form_data()
@@ -40,15 +46,29 @@ def write():
             return render_template('write.html', post=form_data)
         
         db.posts.insert_one(form_data)
-        return redirect(url_for('main'))
+        return redirect(url_for('main.index'))
         
     return render_template('write.html')
 
 @write_bp.route('/edit/<id>', methods=['GET', 'POST'])   #글 수정
+@login_required
 def edit(id):
+    try:
+        object_id = ObjectId(id)
+    except InvalidId:
+        abort(404)
+
+    # 로그인한 사용자가 작성한 글만 조회 가능
+    post = db.posts.find_one({
+        "_id": object_id,
+        "user_id": g.user_id,
+    })
+    if not post:
+        abort(404)
+
     #GET으로 기존 글 정보 로드
     if request.method == 'GET':
-        post = db.posts.find_one({'_id': ObjectId(id)})
+        post = db.posts.find_one({'_id': ObjectId(id), "user_id": g.user_id})   # user id
         if post and 'tags' in post:
             post['tags_str'] = '#' + ' #'.join(post['tags'])
         return render_template('write.html', post=post)
@@ -59,12 +79,12 @@ def edit(id):
         # 태그칸에#만 쓰고 넘기면
         if len(form_data['tags']) == 0:
             flash('태그를 입력해주세요. (예: #1#2)')
-            post = db.posts.find_one({'_id': ObjectId(id)})
+            post = db.posts.find_one({'_id': ObjectId(id), "user_id": g.user_id})
             if post:
                 post.update(form_data)
             return render_template('write.html', post=post)
         
-        db.posts.update_one(
+        result = db.posts.update_one(
             {'_id': ObjectId(id)},
             {'$set': {
                 'title': form_data['title'],
@@ -72,5 +92,9 @@ def edit(id):
                 'tags': form_data['tags'],
                 'summary': form_data['summary']
             }}
-)
-        return redirect(url_for('main'))
+        )
+
+        if result.matched_count == 0:
+            return "수정 권한이 없거나 게시글이 없습니다.", 403
+
+        return redirect(url_for("post.view_post", id=id))
