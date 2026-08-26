bb661ba37d7c4a4fd1153a6a161a94eaf38446df
이종민 <145272576+steinsring@users.noreply.github.com>
2026-08-26 21:19:26 +0900

    fix : conflict

--- db/__init__.py
+++ db/__init__.py
@@ -6,7 +6,7 @@ from db.collections import (
 )
 from db.mongo import close_mongo_client, get_database, get_mongo_client
 
-
+# DB 내부 구현에 있는 다른 변수나 함수는 숨기고, 이 목록만 가져다 쓰게 함
 __all__ = (
     "close_mongo_client",
     "ensure_database_indexes",
